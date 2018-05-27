853c4be9046da1b31812e561ee3f5ca8fb7c9fc7
seongwonlee <swlee@wisoft.io>
2018-05-27 23:24:20 +0900

    code refactoring

--- MPU9250_testbed/MPU9250_testbed.ino
+++ MPU9250_testbed/MPU9250_testbed.ino
@@ -1,17 +1,16 @@
-
+#include <ArduinoJson.h>
 #include "Wire.h"
-
 #include "I2Cdev.h"
 #include "MPU9250.h"
 
 // base voltage ADC value of shock sensor
 const float Volt2G = 0.00537;
 
-
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
+
 MPU9250 accelgyro;
 I2Cdev   I2C_M;
 
@@ -26,7 +25,6 @@ void calcAngleFromGyro(int dt);
 void getShockSensorValue() ;
 
 uint8_t buffer_m[6];
-
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 int16_t mx, my, mz;
@@ -74,22 +72,21 @@ float altitude;
 int time_last = 0;
 int deltatime = 0;
 
-void setup()
-{
+void setup() {
     Wire.begin();
-    Serial.begin(9600);                        
+    Serial.begin(9600);    // serial 통신 9600bps                    
 
-    Serial.println("{""status"":""initialising""}");
     accelgyro.initialize();
     // verify connection
     // Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "{""status"":""successful""}" : "{""status"":""failed""}");
-    delay(100);
+    
+    delay(1000);
     Serial.println("     ");
     // Calibration. device must not move or get force while calibrating
     // Kalman Filter will be added soon
     // calibrate Accelerometer
-    Serial.println("{""calibration"":""calibrating""}");
+    // Serial.println("{""calibration"":""calibrating""}");
+    
     getAccel_Data();
     Axyz_calib[0] = Axyz[0];
     Axyz_calib[1] = Axyz[1];
@@ -99,12 +96,12 @@ void setup()
     Gxyz_calib[0] = Gxyz[0];
     Gxyz_calib[1] = Gxyz[1];
     Gxyz_calib[2] = Gxyz[2];
+    
     // calibrate Initial Angle from Accelerometer
     /*
      * not implemented
      */
-
-    
+     
     // calibrate piezo
     piezo_calib = analogRead(A0);
     
@@ -114,8 +111,7 @@ void setup()
 }
 
 
-void loop()
-{
+void loop() {
     // get data from sensors
     getAccel_Data();
     getGyro_Data();
@@ -128,55 +124,39 @@ void loop()
     deltatime = millis() - time_last;
     time_last = millis();
     calcAngleFromGyro(deltatime);
-    
-    //// output below here
-    Serial.print("Time: ");
-    Serial.print(millis() );
-    Serial.println(" ");
-    
-    Serial.println("Acceleration(g) of X,Y,Z:");
-    Serial.print("x축:");
-    Serial.print(Axyz[0]);
-    Serial.print("\t");
-    Serial.print("y축:");
-    Serial.print(Axyz[1]);
-    Serial.print("\t");
-    Serial.print("z축:");
-    Serial.print(Axyz[2]);
-    Serial.print("\n");
-    
-    
-    Serial.println("Gyro(degress/s) of X,Y,Z:");
-    Serial.print(Gxyz[0]);
-    Serial.print(",");
-    Serial.print(Gxyz[1]);
-    Serial.print(",");
-    Serial.println(Gxyz[2]);
 
-    Serial.println("Angle(degree) of X/Y/Z:");
-    Serial.print(Wxyz[0]);
-    Serial.print(",");
-    Serial.print(Wxyz[1]);
-    Serial.print(",");
-    Serial.println(Wxyz[2]);
-    
-    
-    Serial.println("충격센서 Raw, shock: ");
-    Serial.print( piezo ); // Raw Data From shock sensor
-    Serial.print("\t");
-    Serial.println( shock ); // calced Shock by sensor. Analog Piezo Shock Sensor. 핀번호 A0
-    
+    //JSON Parsing... 
+    StaticJsonBuffer<700> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+
+    JsonArray& acceleration = root.createNestedArray("acceleration");
+    acceleration.add(Axyz[0]);
+    acceleration.add(Axyz[1]);
+    acceleration.add(Axyz[2]);
+
+    JsonArray& gyro = root.createNestedArray("gyro");
+    gyro.add(Gxyz[0]);
+    gyro.add(Gxyz[1]);
+    gyro.add(Gxyz[2]);
+
+    JsonArray& angle = root.createNestedArray("angle");
+    angle.add(Wxyz[0]);
+    angle.add(Wxyz[1]);
+    angle.add(Wxyz[2]); 
+
+    root["attack"] = shock;    // calced Shock by sensor. Analog Piezo Shock Sensor. 핀번호 A0
+
+    root.printTo(Serial);
+    Serial.print("\n");
     delay(1000);
 }
 
-
-void getHeading(void)
-{
+void getHeading(void) {
     heading = 180 * atan2(Mxyz[1], Mxyz[0]) / PI;
     if (heading < 0) heading += 360;
 }
-void getTiltHeading(void)
-{
+
+void getTiltHeading(void) {
     float pitch = asin(-Axyz[0]);
     float roll = asin(Axyz[1] / cos(pitch));
 
@@ -186,8 +166,8 @@ void getTiltHeading(void)
     tiltheading = 180 * atan2(yh, xh) / PI;
     if (yh < 0)    tiltheading += 360;
 }
-void Mxyz_init_calibrated ()
-{
+
+void Mxyz_init_calibrated () {
 
     Serial.println(F("Before using 9DOF,we need to calibrate the compass frist,It will takes about 2 minutes."));
     Serial.print("  ");
@@ -211,10 +191,9 @@ void Mxyz_init_calibrated ()
     Serial.println(mz_centre);
     Serial.println("    ");
 }
-void get_calibration_Data ()
-{
-    for (int i = 0; i < sample_num_mdate; i++)
-    {
+
+void get_calibration_Data () {
+    for (int i = 0; i < sample_num_mdate; i++) {
         get_one_sample_date_mxyz();
 
         if (mx_sample[2] >= mx_sample[1])mx_sample[1] = mx_sample[2];
@@ -237,30 +216,30 @@ void get_calibration_Data ()
     my_centre = (my_max + my_min) / 2;
     mz_centre = (mz_max + mz_min) / 2;
 }
-void get_one_sample_date_mxyz()
-{
+
+void get_one_sample_date_mxyz() {
     getCompass_Data();
     mx_sample[2] = Mxyz[0];
     my_sample[2] = Mxyz[1];
     mz_sample[2] = Mxyz[2];
 }
-void getAccel_Data(void)
-{
+
+void getAccel_Data(void) {
     accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
     Axyz[0] = ((double) ax / 16384 ) - Axyz_calib[0];
     Axyz[1] = ((double) ay / 16384 ) - Axyz_calib[1];
     Axyz[2] = ((double) az / 16384 ) - Axyz_calib[2];
 }
-void getGyro_Data(void)
-{
+
+void getGyro_Data(void) {
     accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
 
     Gxyz[0] = ((double) gx * 250 / 32768 ) - Gxyz_calib[0];
     Gxyz[1] = ((double) gy * 250 / 32768 ) - Gxyz_calib[1];
     Gxyz[2] = ((double) gz * 250 / 32768 ) - Gxyz_calib[2];
 }
-void getCompass_Data(void)
-{
+
+void getCompass_Data(void) {
     I2C_M.writeByte(MPU9150_RA_MAG_ADDRESS, 0x0A, 0x01); //enable the magnetometer
     delay(10);
     I2C_M.readBytes(MPU9150_RA_MAG_ADDRESS, MPU9150_RA_MAG_XOUT_L, 6, buffer_m);
@@ -273,8 +252,8 @@ void getCompass_Data(void)
     Mxyz[1] = (double) my * 1200 / 4096;
     Mxyz[2] = (double) mz * 1200 / 4096;
 }
-void getCompassDate_calibrated ()
-{
+
+void getCompassDate_calibrated () {
     getCompass_Data();
     Mxyz[0] = Mxyz[0] - mx_centre;
     Mxyz[1] = Mxyz[1] - my_centre;
